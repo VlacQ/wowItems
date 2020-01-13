5d8f5281a41597c0445dafdce16137c4c246bc87
Kamil Święch <kamil.swiech@cgi.com>
2020-01-13 13:05:43 +0100

    Export Added

--- src/main/java/com/wowItemsAPI/util/Excel.java
+++ src/main/java/com/wowItemsAPI/util/Excel.java
@@ -2,24 +2,32 @@ package com.wowItemsAPI.util;
 
 import com.wowItemsAPI.entity.Item;
 import com.wowItemsAPI.entity.Price;
-import lombok.Data;
+import com.wowItemsAPI.service.ItemService;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 
-@Data
 @Configuration
-public class ExcelReader {
+public class Excel {
+    private ItemService itemService;
+
+    @Autowired
+    public Excel(ItemService itemService){
+        this.itemService = itemService;
+    }
+
     public List<Item> readExcelFile(MultipartFile file){
         File tempFile;
         Workbook wb;
@@ -89,4 +97,53 @@ public class ExcelReader {
         return false;
     }
 
+    public String exportToFile(){
+        Workbook workbook = new XSSFWorkbook();
+        Sheet sheet = workbook.createSheet();
+        File file;
+        String fileName;
+        SimpleDateFormat dateTime = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss");
+
+        List<Item> items = itemService.findAll();
+
+        int rowCount = 0;
+
+        for (Item item : items) {
+            Row row = sheet.createRow(++rowCount);
+            writeItem(item, row);
+        }
+
+        fileName = "export_" + dateTime.format( new Date()) + ".xlsx";
+        file = new File(fileName);
+
+        try (FileOutputStream outputStream = new FileOutputStream(file)) {
+            workbook.write(outputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return fileName;
+    }
+
+    private void writeItem(Item item, Row row) {
+        Cell cell = row.createCell(0);
+        cell.setCellValue(item.getName());
+
+        writePrice(item.getPriceList(), row, cell);
+    }
+
+    private void writePrice(List<Price> list, Row row, Cell cell) {
+        SimpleDateFormat date = new SimpleDateFormat("yyyy.MM.dd");
+        int counter = 1;
+
+        for (Price price:list) {
+            cell = row.createCell(counter);
+            cell.setCellValue(date.format(price.getDate()));
+            cell = row.createCell(counter+1);
+            cell.setCellValue(price.getQuantity());
+            cell = row.createCell(counter+2);
+            cell.setCellValue(Double.valueOf(String.valueOf(price.getAmount())));
+            counter += 3;
+        }
+    }
 }
